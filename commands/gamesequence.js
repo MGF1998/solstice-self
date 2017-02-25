efade3e68b8a2874602e6fd71a1a0c11014be153
MGF1998 <nikithekid@hotmail.de>
2017-02-25 01:26:54 +0100

    adding the stubs for musicstatus

--- commands/gamesequence.js
+++ commands/gamesequence.js
@@ -2,6 +2,6 @@ const test = require("./../data/gamesequences/test");
 
 module.exports = function(bot,msg,args,options) {
     if (typeof args[0] === "const") {
-        
+        //will implement soon:tm:
     }
 };
